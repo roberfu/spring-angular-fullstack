7c7f0f104c1c7aef00f125fe18166b3bdc07f28b
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-02-08 00:40:01 -0300

    Init backend project

---
+++ springular-backend/src/main/java/cl/springmachine/springular/backend/SpringularBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.springmachine.springular.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringularBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringularBackendApplication.class, args);
+	}
+
+}
